36cbcd10b8406f96a1642f67b7607f9637ac986e
prcv <prcv@cisco.com>
2020-10-10 11:46:57 +0530

    Added my network connections

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './MyInfo.css'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import WebIcon from '@material-ui/icons/Web';
 
 function MyInfo() {
     return (
@@ -25,8 +28,14 @@ function MyInfo() {
                     <a href="https://silly-wescoff-4f0910.netlify.app/" target="_blank">  <div className="myimage"></div></a>
                 </p>
                 <p>
-                    <a href="https://github.com/PrasanthReddy-Chittapu6683"
-                        style={{ color: 'white' }} target="_blank">  <ChevronRightIcon /><ChevronRightIcon /><ChevronRightIcon /><ChevronRightIcon />Click Here for Github link  <ChevronLeftIcon /><ChevronLeftIcon /><ChevronLeftIcon /><ChevronLeftIcon /></a>
+                    <a href="https://github.com/PrasanthReddy-Chittapu6683" title="Github"
+                        style={{ color: 'white', margin: "10px" }} target="_blank"> <GitHubIcon /></a>
+                    <a href="https://www.linkedin.com/in/prasanth-kumar-reddy-cv-385768b5/" title="LinkedIn"
+                        style={{ color: 'white', margin: "10px" }} target="_blank"> <LinkedInIcon /></a>
+                    <a href="https://silly-wescoff-4f0910.netlify.app/" title="My Portfolio"
+                        style={{ color: 'white', margin: "10px" }} target="_blank"> <WebIcon /></a>
+                    {/* <a href="https://github.com/PrasanthReddy-Chittapu6683"
+                        style={{ color: 'white' }} target="_blank">  <ChevronRightIcon /><ChevronRightIcon /><ChevronRightIcon /><ChevronRightIcon />Click Here for Github link  <ChevronLeftIcon /><ChevronLeftIcon /><ChevronLeftIcon /><ChevronLeftIcon /></a> */}
                 </p>
             </div>
         </div>
